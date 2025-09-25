0f7d59fc728a45889e53ae5634f0df7de05dbe02
jdavifranco <jdavifranco@gmail.com>
2025-09-24 21:49:03 -0300

    add person and company details endpoint

--- lib/domain/search/repository/search_repository.dart
+++ lib/domain/search/repository/search_repository.dart
@@ -3,6 +3,8 @@ import 'package:moviealike/data/network_client/request_error.dart';
 import 'package:moviealike/data/search/models/search_type.dart';
 import 'package:moviealike/domain/search/models/search_filter.dart';
 import 'package:moviealike/domain/search/models/search_item.dart';
+import 'package:moviealike/domain/search/models/person_details.dart';
+import 'package:moviealike/domain/search/models/company_details.dart';
 import 'package:result_type/result_type.dart';
 
 abstract class SearchRepository {
@@ -13,4 +15,8 @@ abstract class SearchRepository {
       required SearchFilter filter,
       required String query,
       int page = 1});
+  CancelableOperation<Result<PersonDetails, RequestError>> getPersonDetails(
+      int personId);
+  CancelableOperation<Result<CompanyDetails, RequestError>> getCompanyDetails(
+      int companyId);
 }
